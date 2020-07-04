b4bf17be001b995e5132b4533c4ee0d2a2b4dd91
Shawn <codingshawn@gmail.com>
2020-07-04 20:59:47 +0800

    Created Sqaure class to represents squares on chessboard

---
+++ square.rb
@@ -0,0 +1,10 @@
+class Square
+  attr_reader :column, :row
+  attr_accessor :value
+
+  def initialize(column, row, value = " ")
+    @column = column
+    @row = row
+    @value = value
+  end
+end
