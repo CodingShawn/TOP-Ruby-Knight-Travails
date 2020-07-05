90e9cf89fd1cbacf08face0fbe5216c0dd7bd46c
Shawn <codingshawn@gmail.com>
2020-07-05 13:00:24 +0800

    Modified Chessboard class to able to call on KnightTree class to determine shortest path from one location to another

--- chessboard.rb
+++ chessboard.rb
@@ -1,11 +1,24 @@
 require_relative 'square'
+require_relative 'knighttree'
 
 class Chessboard
   def initialize
     for column in 0..7
       for row in 0..7
-        square.New(column, row)
+        Square.new(column, row)
       end
     end
   end
+
+  def knight_moves(initial_location, end_location)
+    knight_travels = KnightTree.new(initial_location)
+    route = knight_travels.breadth_order_search(end_location)
+    puts " You made it in #{route.length - 1} moves! Heres your path:"
+    for i in route
+      print "#{i}\n"
+    end
+  end
 end
+
+x = Chessboard.new
+x.knight_moves([3,3],[4,3])
